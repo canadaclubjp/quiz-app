9bfafff4e1f4b1b07bb96fea6ca942f0e0ddbcf0
Arlen <arlen@iMac.local>
2025-05-18 11:14:14 +0900

    Increment CSS version to v4 to bust cache

--- src/QuizApp.js
+++ src/QuizApp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './QuizApp.css?v=2';
+import './QuizApp.css?v=4';
 
 export default function QuizApp() {
     const [quiz, setQuiz] = useState(null);
