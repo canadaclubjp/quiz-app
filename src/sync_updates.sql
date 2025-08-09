9a2a5290bc99b515421435ccc3c4ffa500257e07
canadaclubjp <canadaclubjp@bloom.ocn.ne.jp>
2025-08-09 17:33:03 +0900

    Commit local changes from troubleshooting session

---
+++ src/sync_updates.sql
@@ -0,0 +1,8 @@
+INSERT INTO quizzes VALUES(7,'Example Online Test','','2025-07-31 12:06:48.483887');
+INSERT INTO quizzes VALUES(8,'Example Online Test','','2025-07-31 21:31:55.935360');
+INSERT INTO quizzes VALUES(9,'asdasdasd','','2025-07-31 21:37:24.584974');
+INSERT INTO quizzes VALUES(10,'Test Quiz 10','Test description','2025-08-03 15:24:04.855507');
+INSERT INTO questions VALUES(54,7,'What is the teacher''s name for this class?','["Matt", "Gerry", "David", "Suzanne", "Arlen", "Haru"]','["Arlen"]',0,NULL,'','','');
+INSERT INTO questions VALUES(55,8,'What is the teacher''s name for this class?','["Matt", "Gerry", "David", "Suzanne", "Arlen", "Haru"]','["Arlen"]',0,NULL,'','','');
+INSERT INTO questions VALUES(56,9,'asdasdasd','["asdasd", "aasdasd"]','["asdasd"]',0,NULL,'','','');
+INSERT INTO questions VALUES(57,10,'Test question','["Yes", "No"]','["Yes"]',0,NULL,'','','');
